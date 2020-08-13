0d647c512c65f31e39aef542a40996f63b399c1c
peter <peter@berners-lee.Home>
2020-08-13 12:49:49 +1000

    Task 6.1 part A added

---
+++ Task_6_1/SumExists.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Task_6_1
+{
+    class SumExists
+    {
+        public static bool Check(int[] numbers, int sum)
+        {
+            if (numbers is null) throw new ArgumentNullException();    // 1 operation
+            Array.Sort(numbers);                                       // O(n log n)
+            int i = 0;                                                 // 1 operation
+            int j = numbers.Length - 1;                                // 1 operation
+            while (i < j)                                              // n in worst case                                       
+            {
+                if (numbers[i] + numbers[j] == sum) return true;       // n - 1 in worst case
+                else if (numbers[i] + numbers[j] < sum) i++;           // n - 1 in worst case
+                else j--;                                              // n - 1 in worst case
+            }
+            return false;                                              // 1 operation
+        }
+    }
+}
---
+++ Task_6_1/Tester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_6_1
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {10, 5, 6, 8, 2, 19, 9, 11, 15, 1, -8, 50};
+
+            Console.WriteLine(SumExists.Check(numbers, 15));  // true
+            Console.WriteLine(SumExists.Check(numbers, 26));  // true
+            Console.WriteLine(SumExists.Check(numbers, -2));  // true
+            Console.WriteLine(SumExists.Check(numbers, 50));  // false
+            Console.WriteLine(SumExists.Check(numbers, 0));   // true
+            Console.WriteLine(SumExists.Check(numbers, 42));  // true
+            Console.WriteLine(SumExists.Check(numbers, 69));  // true
+        }
+    }
+}
