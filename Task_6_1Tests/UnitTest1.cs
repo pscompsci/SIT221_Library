75fbb68ec4d8e91c68617b5fa1f08549717b646d
peter <peter@berners-lee.Home>
2020-08-27 13:16:07 +1000

    Added Stack implementations to Task 6.1

--- Task_6_1Tests/UnitTest1.cs
+++ Task_6_1Tests/UnitTest1.cs
@@ -8,23 +8,22 @@ namespace Task_6_1Tests
     {
         int[] numbers = {10, 5, 6, 8, 2, 19, 9, 11, 15, 1, -8, 50};
 
-
         [Theory]
-        [InlineData(15)]
-        [InlineData(26)]
-        [InlineData(-2)]
-        [InlineData(0)]
-        [InlineData(42)]
-        [InlineData(69)]
-        [InlineData(1)]
+        [InlineData(-2)]   // 6 + -8
+        [InlineData(0)]    // 8 + -8
+        [InlineData(1)]    // 9 + -8
+        [InlineData(15)]   // 10 + 5
+        [InlineData(26)]   // 15 + 11
+        [InlineData(42)]   // 50 + -8
+        [InlineData(69)]   // 50 + 18
         public void Test1(int sum)
         {
             Assert.True(SumExists.Check(numbers, sum));
         }
 
         [Theory]
-        [InlineData(50)]
         [InlineData(-100)]
+        [InlineData(50)]
         [InlineData(100)]
         public void Test2(int sum)
         {
