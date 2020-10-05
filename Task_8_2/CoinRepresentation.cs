92c7ec8ffaeefddc47ed9d07485755aad07b2799
peter <peter@berners-lee.Home>
2020-10-05 23:51:20 +1100

    Updated 8.2

--- Task_8_2/CoinRepresentation.cs
+++ Task_8_2/CoinRepresentation.cs
@@ -1,4 +1,9 @@
-﻿﻿using System;
+﻿﻿// Student Name: Peter Stacey
+// Student ID: 219011171
+//
+// Task 8.2HD
+
+using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections;
@@ -7,12 +12,55 @@ using System.Linq;
 
 namespace CoinRepresentation
 {
+    /// <summary>
+    /// Provices a static method to Solve the total number of combinations
+    /// that exist in a set of coins of 2^k denominations.
+    /// </summary>
     public class CoinRepresentation
     {
-        public static long Solve(List<long> numbers, long target, List<long> partial)
+        private static Hashtable _cache = new Hashtable();
+
+        /// <summary>
+        /// Recursively solves the total number of combinations possible for
+        /// a limited set of coins (two each) of 2^k denominations and returns
+        /// the sum.
+        /// 
+        /// Counts duplicate solutions as a single solution, so that the total
+        /// returned is the total number of unique combuinations to the problem.
+        /// 
+        /// For example, for the sum 6, the set of coins includes:
+        /// 2^0, 2^1, 2^2, 2^3, 2^4, 2^5 and 2^6, or a set containing
+        /// (1, 1, 2, 2, 4, 4, 8, 8, 16, 16, 32, 32, 64, 64)
+        /// 
+        /// The solution optimises the problem space to ignore coins
+        /// that exceed the total.
+        /// 
+        /// For the example total of 6, 3 is returned, for
+        /// the combinations:
+        /// 4 + 1 + 1
+        /// 4 + 2
+        /// 2 + 2 + 1 + 1
+        /// </summary>
+        /// <param name="sum">The sum as a long, to find combinations for</param>
+        /// <returns></returns>
+        public static long Solve(long sum)
         {
-            // Not uploadfing until the end of semster
-            throw new NotImplementedException();
+            if (sum < 0) return 0; // No coins and no sum possible
+
+            if (sum is 0) return 1; // Complete combination found
+            
+            if(!_cache.ContainsKey(sum))
+            {
+                if (sum % 2 is 0)
+                {
+                    _cache.Add(sum, Solve(sum / 2) + Solve(sum / 2 - 1));
+                }
+                else
+                {
+                    _cache.Add(sum, Solve((sum - 1) / 2));
+                }
+            } 
+            return Convert.ToInt64(_cache[sum]);           
         }
     }
 }
--- Task_8_2/Tester.cs
+++ Task_8_2/Tester.cs
@@ -1,59 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-
-namespace CoinRepresentation
-{
-    class Tester
-    {
-        static void Main(string[] args)
-        {
-            List<int> correct = new List<int>(TestGenerator.Count());
-            List<int> incorrect = new List<int>(TestGenerator.Count());
-            int scores = 0;
-
-            for (int i = 0; i < TestGenerator.Count(); i++)
-            {
-                try
-                {
-                    long sum = 0;
-                    long result = TestGenerator.Generate(i, out sum);
-                    Console.WriteLine("\nAttempting test instance {0} with {1} as the argument and {2} as the expected answer", i, sum, result);
-                    Stopwatch watch = new Stopwatch();
-                    watch.Start();
-                    long answer = CoinRepresentation.Solve(sum);
-
-                    if (result == answer)
-                    {
-                        scores++;
-                        correct.Add(i);
-                        // Console.WriteLine(" :: SUCCESS (Time elapsed {0})", watch.Elapsed);
-                    }
-                    else
-                    {
-                        incorrect.Add(i);
-                        // Console.WriteLine(" :: FAILED with an incorrect answer of {0}",answer);
-                    }
-                }
-                catch (Exception e)
-                {
-                    incorrect.Add(i);
-                    Console.WriteLine(" :: FAILED with the runtime error {1}", i, e.ToString());
-                }
-            }
-
-            Console.WriteLine("\nSummary: {0} tests out of {1} passed", scores, TestGenerator.Count());
-            Console.WriteLine("Tests passed ({1} to {2}): {0}", correct.Count == 0 ? "none" : string.Join(", ", correct), 0, TestGenerator.Count());
-            Console.WriteLine("Tests failed ({1} to {2}): {0}", incorrect.Count == 0 ? "none" : string.Join(", ", incorrect), 0, TestGenerator.Count());
-
-            // Console.ReadKey();
-        }
-
-    }
-}
-=======
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -101,9 +46,8 @@ namespace CoinRepresentation
             Console.WriteLine("Tests passed ({1} to {2}): {0}", correct.Count == 0 ? "none" : string.Join(", ", correct), 0, TestGenerator.Count());
             Console.WriteLine("Tests failed ({1} to {2}): {0}", incorrect.Count == 0 ? "none" : string.Join(", ", incorrect), 0, TestGenerator.Count());
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
     }
 }
->>>>>>> 938f432e49a79c2e73bc75be1132a1ae614a480d
